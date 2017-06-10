0d662ff23dc78c263133feff27bad8982b72a7c3
Dominika Gnich <d.gnich@wp.pl>
2017-06-10 12:13:37 +0200

    warunek if

---
+++ js/warunki.js
@@ -0,0 +1,13 @@
+'use strict';
+//instrukcje warunkowe if, if-else, else-if, switch //
+
+var wzrostMateusza = 190;
+var wzrostOli = 170;
+
+//warunek if//
+
+if ( wzrostOli < wzrostMateusza){
+    console.log("Ola jest niższa");
+}
+
+
